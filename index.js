eb00b2defb127471f06dd32a716aabf53ff5413b
herve colin <herve.colin@nantes.imie.fr>
2017-04-14 12:44:29 +0200

    version message pour un user emit

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const uuid = require('uuid/v4');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-
-const port = 8080;
+const settings = require('./settings');
 
 // Instanciation du serveur web, de Express et de Socket.io
 const app = express();
@@ -11,48 +10,58 @@ const server = http.Server(app);
 const io = socketio(server);
 
 // Lancement du serveur HTTP
-server.listen(port, () => {
-    console.log('Listening on port ' + port);
+server.listen(settings.port, () => {
+  console.log('Listening on port ' + settings.port);
 });
 
 // Contenu du dossier public accessible sur le web
 app.use('/', express.static(__dirname + '/public'));
 
-const users=[];            // déclaration de ma liste users avant Connexion !
-
+// Liste des connectés
+const users = [];
 
 // Connexion des clients socket.io
 io.on('connection', (socket) => {
+  console.log('User (' + socket.id + ') vient de se connecter');
 
-    console.log('User (' + socket.id + ') vient de se connecter');
+  // Ajout d'un connecté
+  const user = {
+    id: socket.id,
+    // Nom au hasard dans la liste des noms par défaut
+    nickname: settings.defaultNicknames[Math.floor(Math.random() * settings.defaultNicknames.length)]
+  };
+  users.push(user);
 
-    const user = {       //creation d'un user !
-      id:socket.id,
-      name:socket.id
-    };
-    users.push(user);      //pour ajouter un utilisateur ou un element dans io indexOf trouver un element splice retirer un ou plusieur éléments
-    // const message = {
-    //  id: uuid(),
-    // userId: user.id,
-    // date: new date.getTime(),
-    // txt: message.txt
-    // socket.broadcast.emit(name, date);
-    // };
+  // Diffusion de la liste de connectés à tout le monde
+  io.emit('users', users);
+
+  // Déconnexion de l'utilisateur
+  socket.on('disconnect', () => {
+    console.log('User (' + socket.id + ') vient de se déconnecter');
+    // Suppression du user de la liste
+    users.splice(users.indexOf(user), 1);
+    // Diffusion de la liste de connectés à tout le monde
     io.emit('users', users);
+  });
 
+  // Réception d'un nouveau message
+  socket.on('msg', (txt) => {
+    // Nouvel objet message avec un id et une date
+    const message = {
+      id: uuid(),
+      userId: user.id,
+      date: new Date().getTime(),
+      txt
+    };
+    // Diffusion du message auprès de tous les connectés
+    io.emit('msg', message);
+  });
 
+  // Réception d'un event "nick"
+  // pour changer de pseudo
+  socket.on('nick', (nickname) => {
+    user.nickname = nickname;
+    io.emit('users', users);
+  });
 
-    // Déconnexion de l'utilisateur
-    socket.on('disconnect', () => {
-
-        console.log('User (' + socket.id + ') vient de se déconnecter');
-        users.splice(users.indexOf(user), 1);  //enlever l'utilisateur lors de la deconnect
-        io.emit('users', users);
-    });
-
-    // Réception d'un nouveau message
-    socket.on('msg', (txt) => {
-        // Diffusion du message auprès de tous les connectés
-        io.emit('msg', txt);
-    });
 });
--- public/client.js
+++ public/client.js
@@ -4,33 +4,89 @@ var socket = io();
 
 // Log lorsqu'on est bien connecté
 socket.on('connect', function() {
-    console.log('Connecté !');
+  console.log('Connecté !');
 });
 
 // Réception d'un nouveau message
 // et ajout dans la liste
-socket.on('msg', function(txt) {
-    var messages = document.getElementById('messages');
-    var li = document.createElement('li');
-    li.innerText = txt;
-    messages.appendChild(li);
+socket.on('msg', function(message) {
+  // Récupération du user expéditeur du message et de son nickname
+  var user = users.filter(function(user) {
+    return user.id === message.userId;
+  })[0];
+  var nickname = user ? user.nickname : '😄';
+
+  // Ajout du message à la liste
+  var messages = document.getElementById('messages');
+  var li = document.createElement('li');
+  li.innerText = nickname + ': ' + message.txt;
+  messages.appendChild(li);
+
+  // Scroll en bas de la liste
+  messages.scrollTop = messages.scrollHeight - messages.clientHeight;
 });
 
 // Réception de la nouvelle liste des connectés
-// de type [{id, name}, ...]
-
+// de type [{id, nickname}, ...]
 var usersUl = document.querySelector('#users ul');
-socket.on('users', function(users) {
-    usersUl.innerHTML = users.map(u => '<li>' + u.name + '</li>').join('')  //function pour récupérer name utilisateur
-    console.log(users);
+var users = [];
+socket.on('users', function(_users) {
+  // Liste des anciens ids de users pour supprimer les <div> des users déconnectés
+  var oldIds = users.map(function(u) {
+    return u.id;
+  });
+
+  users = _users;
+  usersUl.innerHTML = _users.map(function(u) {
+    return '<li>' + u.nickname + '</li>'
+  }).join('');
+
+  // Affichage des users à l'écran selon leurs positions
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    // Suppression de l'id du user de la liste des anciens ids
+    var oldIdIndex = oldIds.indexOf(user.id);
+    if (oldIdIndex !== -1) {
+      oldIds.splice(oldIdIndex, 1);
+    }
+    var userDiv = document.getElementById('user-' + user.id);
+    // Si le <div> du user n'existe pas encore, on le crée
+    if (!userDiv) {
+      userDiv = document.createElement('div');
+      userDiv.id = 'user-' + user.id;
+      userDiv.className = 'user';
+      userDiv.innerText = user.nickname;
+      document.body.appendChild(userDiv);
+    }
+    user.position = {x: Math.random() * 100, y: Math.random() * 100};
+    userDiv.style.left = user.position.x + '%';
+    userDiv.style.top = user.position.y + '%';
+  }
+
+  // Suppression des anciens users
+  for (var i = 0; i < oldIds.length; i++) {
+    var userDiv = document.getElementById('user-' + oldIds[i]);
+    if (userDiv) {
+      userDiv.parentNode.removeChild(userDiv);
+    }
+  }
 });
 
 // Envoi d'un nouveau message
 var msgform = document.getElementById('msgform');
 msgform.addEventListener('submit', function(e) {
-    e.preventDefault();
-    socket.emit('msg', this.message.value);
-    this.message.value = '';
+  e.preventDefault();
+  // Texte du champ
+  var txt = this.message.value;
+  // Commande /nick pour changer de pseudo
+  if (txt.indexOf('/nick ') === 0) {
+    var nickname = txt.substr(6);
+    socket.emit('nick', nickname);
+  } else {
+    // Envoi d'un message normal
+    socket.emit('msg', txt);
+  }
+  this.message.value = '';
 });
 
 // Autofocus sur le champ de texte
